dd2370b8a36f07849622b5410e1258d02d58d492
Henri Niinepuu <henriniinepuu@gmail.com>
2024-11-04 18:51:32 +0200

    Added user logout API that will just delete the cookie

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -3,19 +3,31 @@ import { zValidator} from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
 import { CreateAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
-import { setCookie } from "hono/cookie";
+import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
     .post(
         "/login", 
         zValidator("json",loginSchema),
-        (c) => {
+        async (c) => {
             const { email, password} = c.req.valid("json");
+            const { account } = await CreateAdminClient();
 
-            console.log({email , password});
+            const session = await account.createEmailPasswordSession(
+                email,
+                password,
+            );
 
-            return c.json({ email, password})
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30
+            })
+
+            return c.json({ success: true})
     })
     .post(
         "/register",
@@ -25,7 +37,7 @@ const app = new Hono()
             
             const {account} = await CreateAdminClient();
 
-            const user = await account.create(
+            await account.create(
                 ID.unique(),
                 email,
                 password,
@@ -45,9 +57,13 @@ const app = new Hono()
                 maxAge: 60 * 60 * 24 * 30
             })
 
-            return c.json({ hello : "word"});
+            return c.json({ success: true });
         }
 
     )
+    .post("/logout", (c) => {
+        deleteCookie(c, AUTH_COOKIE)
+        return c.json({ succsess: true })
+    })
     
 export default app;
