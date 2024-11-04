1eceabfdedf756cf5dde745ca6bba6bd07f381ea
Henri Niinepuu <henriniinepuu@gmail.com>
2024-11-04 22:45:18 +0200

    Redirect if not authorized

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,11 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {InferRequestType, InferResponseType} from "hono";
 import {client} from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
 type RequestType = InferRequestType<typeof client.api.auth.logout["$post"]>;
 
 export const useLogout = () => {
+    const router = useRouter();
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -17,7 +19,7 @@ export const useLogout = () => {
             return await response.json();
         },
         onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["current"]})
+            router.refresh();
         }
     })
     return mutation;
